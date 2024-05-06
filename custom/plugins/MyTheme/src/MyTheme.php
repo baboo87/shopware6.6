4a1d2244b2e0b3e26a0ccb6c0ce37ddaaffd9d83
Ayoub <ayoubayar1307@gmail.com>
2024-05-06 15:23:08 +0200

    Your commit message

---
+++ custom/plugins/MyTheme/src/MyTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace MyTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class MyTheme extends Plugin implements ThemeInterface
+{
+}
